8333c88a3daf8fc1f270626839dc09bbbcf9a82d
Leandro Ostera <leandro@ostera.io>
2018-11-02 11:44:37 +0100

    +[docs] parametrize mkdocs

--- scripts/mk-docs.sh
+++ scripts/mk-docs.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-readonly PKG=ReActor
+readonly PKG=$1
 
 readonly DOCS=docs
 readonly ODOC=$(which odoc)
-readonly LIB=./lib/bs/src
-readonly SRC=./src
+readonly LIB=$2
+readonly SRC=$3
+readonly LIB_PATTERN=$4
 
 # Gather the sources to compile .odoc files
-readonly CMT_FILES=$(find ${LIB} -name "*.cmti")
+readonly CMT_FILES=$(find ${LIB} -name "${LIB_PATTERN}")
 readonly MLD_FILES=$(find ${SRC} -name "*.mld")
+
+echo "info: 📚 Compiling documentation for package: ${PKG}"
+echo "info: Looking for files matching ${LIB_PATTERN} in ${LIB}"
+echo "info: Looking for files matching *.mld in ${SRC}"
 
 echo "<< Compiling module docs..."
 for file in ${CMT_FILES}; do
